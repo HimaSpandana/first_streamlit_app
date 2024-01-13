836799f7ca68763637312ddc29dd155cbe6441b9
Hima Barla <63376682+HimaSpandana@users.noreply.github.com>
2024-01-12 18:04:15 -0800

    Update streamlit_app.py
    Add multi select to pick fav fruits to add in the smoothie

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,4 +11,9 @@ streamlit.text( '🥑🍞Avacado toast')
 
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+#add the pick list
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+
+#Display table on the page
 streamlit.dataframe(my_fruit_list)
