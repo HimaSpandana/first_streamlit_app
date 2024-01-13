9b22ed8cd707f7f0fadee0e3a473eb2dd5b83699
Hima Barla <63376682+HimaSpandana@users.noreply.github.com>
2024-01-12 18:23:10 -0800

    Update streamlit_app.py
    correct - chose fruit name column as index

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,9 +11,11 @@ streamlit.text( '🥑🍞Avacado toast')
 
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
+
 
 #add the pick list
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index('fruit')))
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
 #chose the fruit name column as index
 
