d474a33a25b8632feb60695f19671f11ed269ea5
Hima Barla <63376682+HimaSpandana@users.noreply.github.com>
2024-01-14 01:00:09 -0800

    Update streamlit_app.py
     Let's Query Our Trial Account Metadata

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,6 +37,14 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # output should be in a single table
 streamlit.dataframe(fruityvice_normalized)
 
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
+
 #chose the fruit name column as index
 
 
