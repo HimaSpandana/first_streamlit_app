5e218cf0c00981c8bdd75526ba183ec068328a8d
Hima Barla <63376682+HimaSpandana@users.noreply.github.com>
2024-01-14 17:29:16 -0800

    Update streamlit_app.py
    add

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,32 +1,33 @@
+
 import streamlit 
+
 import snowflake.connector
-import pandas
-import requests
+
 
 streamlit.title('Opal Ridge healthy diner')
+
 streamlit.header('Breakfast Menu')
+
 streamlit.text(' 🥣 Omega 3 and Blackberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach and Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
-streamlit.text('🥑🍞Avocado toast')
+streamlit.text( '🥑🍞Avacado toast')
 
+import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-# Add the pick list
-fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index), ['Avocado', 'Strawberries'])
+
+#add the pick list
+fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
-# New section to display fruityvice API response
+# New section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
-streamlit.write('The user entered', fruit_choice)
-
-# Define the Snowflake cursor here
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
 
 try:
     my_cur.execute("select * from fruit_load_list")
@@ -36,4 +37,19 @@ try:
 except Exception as e:
     streamlit.error(f"Error executing SQL query: {e}")
 
-# Continue with the rest of your code
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+#streamlit.text(fruityvice_response.json())
+
+
+# Take the json version of response and normalize it 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+
+# output should be in a single table
+streamlit.dataframe(fruityvice_normalized)
+
+
+
+#chose the fruit name column as index
+#Display table on the page
+#streamlit.dataframe(my_fruit_list)
