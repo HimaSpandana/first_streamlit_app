041e88af2751614ba6a9e2a044e77fc12d1a90ae
Hima Barla <63376682+HimaSpandana@users.noreply.github.com>
2024-01-14 18:13:29 -0800

    Update streamlit_app.py
    add

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,6 +29,14 @@ streamlit.write('The user entered ', fruit_choice)
 import snowflake.connector
 
 
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
+
+
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 #streamlit.text(fruityvice_response.json())
@@ -40,12 +48,6 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # output should be in a single table
 streamlit.dataframe(fruityvice_normalized)
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
-streamlit.text(my_data_row)
 
 #chose the fruit name column as index
 #Display table on the page
